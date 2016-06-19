462ded4516314cd99b04fc1e41af8163c8314e73
Gustavo Felizola <gfelizola@gmail.com>
2016-06-19 17:20:02 -0300

    removing unused

--- src/routes.jsx
+++ src/routes.jsx
@@ -4,29 +4,14 @@ import Main  from 'routes/Main';
 import Login from 'routes/Login';
 import Home  from 'routes/Home';
 
-import ProductsHome  from 'routes/products/Home';
-import ProductsForm  from 'routes/products/Form';
-
 import Auth  from 'utils/auth';
 import { Router, Route, IndexRoute, Redirect, hashHistory } from 'react-router';
-
-function validateLogin( nextState, replace ){
-    if ( ! Auth.getToken() ) {
-        replace({
-            pathname: '/login',
-            state: { nextPathname: nextState.location.pathname }
-        });
-    }
-};
 
 export default (
     <Router history={hashHistory}>
         <Route path="/" component={Main}>
-            <IndexRoute component={Home} onEnter={ validateLogin } />
+            <IndexRoute component={Home} />
             <Route path="login" component={Login} />
-            <Route path="products" component={ProductsHome} onEnter={ validateLogin } />
-            <Route path="products/new" component={ProductsForm} onEnter={ validateLogin } />
-            <Route path="products/edit/:id" component={ProductsForm} onEnter={ validateLogin } />
         </Route>
         <Redirect from="*" to="/" />
     </Router>
