7c59f4f3055df84be2fe08abfb88ff47aecdc105
Gustavo Felizola <gfelizola@gmail.com>
2016-06-20 11:24:02 -0300

    passo3 in snippets

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,40 +1,6 @@
-import React          from 'react';
-import { withRouter } from 'react-router';
-
-import { AppBar, Drawer, IconButton, IconMenu, MenuItem } from 'material-ui';
-import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
-
-class Menu extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {open: false};
-    }
-
-    _handleToggle = () => this.setState({open: !this.state.open});
-
-    _handleMenuItem = path => {
-        this.setState({open: false}, () => {
-            this.props.router.push(path)
-        });
-    }
-
-    render() {
-        return (<div>
-            <AppBar
-                title="Meu App React"
-                onLeftIconButtonTouchTap={this._handleToggle}
-                iconElementRight={<IconButton><MoreVertIcon /></IconButton>}
-                />
-
-            <Drawer
-                open={this.state.open}
-                docked={false}
-                onRequestChange={this._handleToggle.bind(this, false)}>
-                <MenuItem onTouchTap={ this._handleMenuItem.bind(this, 'home') }>Home</MenuItem>
-                <MenuItem onTouchTap={ this._handleMenuItem.bind(this, 'other') }>Other page</MenuItem>
-            </Drawer>
-        </div>)
-    }
-}
-
-export default withRouter(Menu);
+/*
+p3-imports
+p3-class
+p3-render
+p3-functions
+*/
