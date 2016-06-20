26777ea50a335e30244d777728461af375752151
Gustavo Felizola <gfelizola@gmail.com>
2016-06-20 13:43:16 -0300

    passo2 snippets

--- src/routes/Page1.jsx
+++ src/routes/Page1.jsx
@@ -1,20 +1,6 @@
-import React from 'react';
-import { Link } from 'react-router';
-
-class Page1 extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <div className="row center-xs">
-                <h1>Other page</h1>
-
-                <Link to="/">Back to home</Link>
-            </div>
-        );
-    }
-}
-
-export default Page1
+/*
+p2-imports
+p2-class
+p2-export
+p2-render
+*/
